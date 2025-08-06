fcdc98811214facb55594190a002b721bc571e29
Abhishek-S0111 <160207083+Abhishek-S0111@users.noreply.github.com>
2025-08-06 22:54:58 +0530

    started streamlit deployement

---
+++ streamlit-deployement/App.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Are you the life of the party or the one ghosting it? Find out here.")
+
+st.caption("We made a model that tries to guess if you're an introvert or extrovert — just answer some questions and let the algorithm judge you.")
+
