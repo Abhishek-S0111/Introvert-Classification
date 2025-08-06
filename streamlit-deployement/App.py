d4477fab1bd3d5ee987fbe5f121f88ff14a594b8
Abhishek-S0111 <160207083+Abhishek-S0111@users.noreply.github.com>
2025-08-07 00:49:46 +0530

    Updated model and streamlit code.

--- streamlit-deployement/App.py
+++ streamlit-deployement/App.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import pandas as pd
+import joblib
+
+def map_bool(data):
+    "Works only for Yes/No form of boolean data"
+    for i in data.columns:
+        if(type(data[i].iloc[0]) == str):
+            data[i] = data[i].map({'Yes':1,'No':0})
 
 st.title("Are you the life of the party or the one ghosting it? Find out here.")
 
@@ -14,3 +21,20 @@ drained_after_socializing = st.radio("Do you feel drained after attending any so
 friend_circle_size = st.number_input("How large is your circle of friends?", 0 , 1000)
 post_frequency = st.number_input("How frequently do you post on social media in a day?", 0, 1000)
 
+data = pd.DataFrame({
+    "Time_spent_Alone":time_spent_alone,
+    "Stage_fear":stage_fear,
+    "Social_event_attendance":social_event_attendance,
+    "Going_outside":going_outside,
+    "Drained_after_socializing":drained_after_socializing,
+    "Friends_circle_size":friend_circle_size,
+    "Post_frequency":post_frequency
+}, index = [1])
+
+map_bool(data)
+
+model = joblib.load("model/introvert_classifier_model.pkl")
+
+pred = model.predict(data)
+
+st.write(f"You are an {"Introvert" if pred else "Extrovert"}")
